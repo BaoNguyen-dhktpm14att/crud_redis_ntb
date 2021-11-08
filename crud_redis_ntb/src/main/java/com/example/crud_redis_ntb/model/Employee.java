0ecf9228348f03215132d64e3ea5b53dd4eebfa2
DESKTOP-1VFJKRB\USER <ngtbao2000@gmail.com>
2021-11-08 10:09:28 +0700

    finish

---
+++ crud_redis_ntb/src/main/java/com/example/crud_redis_ntb/model/Employee.java
@@ -0,0 +1,16 @@
+package com.example.crud_redis_ntb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee implements Serializable {
+
+    private int id;
+    private String name;
+}
---
+++ crud_redis_ntb/src/test/java/com/example/crud_redis_ntb/CrudNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_redis_ntb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
